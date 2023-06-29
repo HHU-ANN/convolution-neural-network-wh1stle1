f57f85d8758851ac8c6f54b34a03e88afd7f5fda
wh1stle1 <106904643+wh1stle1@users.noreply.github.com>
2023-06-29 21:33:05 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -10,12 +10,79 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+import torch.nn.functional as F
 
 from torch.utils.data import DataLoader
     
 
-class NeuralNetwork(nn.Module):
-    pass
+class ResNet(nn.Module):
+        def __init__(self, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(16, 3)
+        self.layer2 = self._make_layer(32, 4, stride=2)
+        self.layer3 = self._make_layer(64, 6, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64, num_classes)
+
+    def _make_layer(self, channels, num_blocks, stride=1):
+        layers = []
+        layers.append(BasicBlock(self.in_channels, channels, stride))
+        self.in_channels = channels
+        for _ in range(num_blocks - 1):
+            layers.append(BasicBlock(channels, channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+# 定义BasicBlock模块
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = None
+        if stride != 1 or in_channels != channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(channels)
+            )
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        return out
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
